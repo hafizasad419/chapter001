91ace31e3747f85681ad172013c568971b12024c
hafizasad419 <hafizasad419@gmail.com>
2024-10-29 19:32:22 +0500

    adjust footer margin

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -7,7 +7,7 @@ import SmoothTicker from '../SmoothTicker';
 const Layout: FC = (): JSX.Element => {
     return (
         <>
-            <div className="flex flex-col min-h-screen mb-12 bg-gradient-to-br from-slate-700 via-navy-600 to-navy-900">
+            <div className="flex flex-col min-h-screen mb-20 bg-gradient-to-br from-slate-700 via-navy-600 to-navy-900">
                 <Navbar />
                 <main className="flex-grow">
                     <Outlet />
